ee7cb014b900e530fd43a003e58b69a9b7b4c9dc
Daniel Contreras Aladro <daniel.contreras.aladro@gmail.com>
2017-09-05 14:32:42 +0100

    #92 : date format and visualization

--- src/components/item-data/item-data.ts
+++ src/components/item-data/item-data.ts
@@ -9,13 +9,13 @@ import { CategoriesService } from '../../providers/categories/categoriesService'
   for more info on Angular 2 Components.
 */
 @Component({
-  selector: "item",
-  templateUrl: "item-data.html",
-  inputs: ["item", "creating"],
+  selector: 'item',
+  templateUrl: 'item-data.html',
+  inputs: ['item', 'creating'],
   providers: [CategoriesService]
   // outputs: ['remove', 'move', 'edit']
 })
-export class Item implements OnInit{
+export class Item implements OnInit {
   @Input() item: any;
   @Input() creating: boolean;
   @Input() icons: any;
@@ -24,28 +24,29 @@ export class Item implements OnInit{
   @Output() edit: EventEmitter<any> = new EventEmitter();
   @Output() save: EventEmitter<any> = new EventEmitter();
 
-  oldMeasurement: string = "";
+  oldMeasurement: string = '';
 
   constructor(private catService: CategoriesService) {}
 
-  ngOnInit(){
-	  this.oldMeasurement = this.item.category.measurement;
+  ngOnInit() {
+    this.oldMeasurement = this.item.category.measurement;
+    console.log(this.item.fechaCaducidad);
   }
 
   changeUnitStep(measurement) {
-    if (measurement === "UNIDADES") {
+    if (measurement === 'UNIDADES') {
       this.item.category.unitStep = 1;
       if (this.creating || measurement !== this.oldMeasurement) {
         this.item.cantidadElemento = 1;
         this.item.cantidadMinima = 1;
       }
-    } else if (measurement === "GRAMOS") {
+    } else if (measurement === 'GRAMOS') {
       this.item.category.unitStep = 100;
       if (this.creating || measurement !== this.oldMeasurement) {
         this.item.cantidadElemento = 100;
         this.item.cantidadMinima = 100;
       }
-    } else if (measurement === "LITROS") {
+    } else if (measurement === 'LITROS') {
       this.item.category.unitStep = 0.25;
       if (this.creating || measurement !== this.oldMeasurement) {
         this.item.cantidadElemento = 1;
@@ -57,9 +58,9 @@ export class Item implements OnInit{
         this.item.cantidadElemento = 1;
         this.item.cantidadMinima = 1;
       }
-	}
-	this.oldMeasurement = this.item.category.measurement;
-   this.save.emit(this.item);
+    }
+    this.oldMeasurement = this.item.category.measurement;
+    this.save.emit(this.item);
   }
 
   onChange(event) {
@@ -68,10 +69,12 @@ export class Item implements OnInit{
 
   plusElement(event, amount) {
     if (amount) {
-      let added: number = this.item.cantidadElemento + this.item.category.unitStep;
+      let added: number =
+        this.item.cantidadElemento + this.item.category.unitStep;
       this.item.cantidadElemento = Math.round(added * 100) / 100;
     } else {
-      let added: number = this.item.cantidadMinima + this.item.category.unitStep;
+      let added: number =
+        this.item.cantidadMinima + this.item.category.unitStep;
       this.item.cantidadMinima = Math.round(added * 100) / 100;
     }
     this.save.emit(this.item);
@@ -93,31 +96,31 @@ export class Item implements OnInit{
   // TODO: check these functions, their need and their functionality
 
   markItem(event) {
-    console.log("markItem");
+    console.log('markItem');
     this.item.marked = !this.item.marked;
     this.save.emit(this.item);
   }
 
   editItem(event) {
-    console.log("editItem:" + JSON.stringify(this.item));
+    console.log('editItem:' + JSON.stringify(this.item));
     this.edit.emit(this.item);
   }
 
   removeItem(event) {
-    console.log("removeItem");
+    console.log('removeItem');
     this.remove.emit(this.item);
   }
 
   moveItem(event) {
-    console.log("moveItem");
+    console.log('moveItem');
     this.move.emit(this.item);
   }
 
   // TODO: request action to do on the category change,unitstep and measurement...do not change amounts
   editCategory(event) {
-    this.catService.changeCategory(this.item.category, this.item).then(data=>{
-		 this.item = data;
-    	this.changeUnitStep(this.item.category.measurement);
-	 });
+    this.catService.changeCategory(this.item.category, this.item).then(data => {
+      this.item = data;
+      this.changeUnitStep(this.item.category.measurement);
+    });
   }
 }
